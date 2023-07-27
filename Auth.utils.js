a7c0e46a17bb1731e7ae400342a238e862cde56d
Solutech11 <soluwizy@gmail.com>
2023-07-27 01:30:07 +0100

    output for auth

--- Auth.utils.js
+++ Auth.utils.js
@@ -5,7 +5,7 @@ function Errordisplay(error) {
     console.log(error.message);// can be removed
         if (error.message) {
             const msg=(error.message.split(':')[2])
-            return {msg:msg?(msg.split(',')[0])?(msg.split(',')[0].split(' ').find(i=>i=='dup'))?'Account details already exist':(msg.split(',')[0]):'Error occured':'Error occured from your end'}
+            return {msg:msg?(msg.split(',')[0])?(msg.split(',')[0].split(' ').find(i=>i=='dup'))?'Details already exist':(msg.split(',')[0]):'Error occured':'Issue occured from your end'}
         } else {
             console.log(error);
             return{msg:'Severe error occured'}
