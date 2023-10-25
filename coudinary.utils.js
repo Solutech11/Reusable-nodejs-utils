d5bb525696a2457ed608f5dabb647b4f4d6887b3
Solutech11 <soluwizy@gmail.com>
2023-10-25 15:55:35 +0100

    update

--- coudinary.utils.js
+++ coudinary.utils.js
@@ -1,9 +1,10 @@
 //cloudinary
 const cloudinary= require('cloudinary')
 cloudinary.config({
-    cloud_name:process.env.cloud_name,
-    api_key:process.env.api_key,
-    api_secret:process.env.api_secret
+    cloud_name:'solutech11',
+    api_key:'195948511332187',
+    api_secret:'d7lpvzOQEgaO5t9S1ld22Vh0srE',
+    secure:true
 })
 
 
@@ -21,6 +22,74 @@ async function uploadimg(file, path) {
     console.log(error);
     return {error:error.message}
   }
+}
+
+async function uploadVideo(file, path) {
+    try {
+      let files = file?file:false
+      let paths = path?path:false
+      if (files!=false && paths!=false) {
+          
+          let upload =await cloudinary.v2.uploader.upload(files.tempFilePath,{resource_type:'video',folder:paths,use_filename:false,unique_filename:true})
+          return {url:upload.secure_url, publicID:upload.public_id}
+      }
+      return {error:'Images are missing'}
+    } catch (error) {
+      console.log(error);
+      return {error:error.message}
+    }
+}
+
+async function uploadRaw(file, path, toppingName) {
+    try {
+      let files = file?file:false
+      let paths = path?path:false
+      if (files!=false && paths!=false) {
+          
+          let upload =await cloudinary.v2.uploader.upload(files.tempFilePath,{resource_type:'raw',folder:paths,filename_override:toppingName+files.name})
+          return {url:upload.secure_url, publicID:upload.public_id}
+      }
+      return {error:'File is missing'}
+    } catch (error) {
+      console.log(error);
+      return {error:error.message}
+    }
+}
+
+async function deleteFile(publicIDs){
+    try {
+        let publicID= publicIDs?publicIDs:false
+        console.log(publicID);
+    if (publicID!=false) {
+        await cloudinary.v2.uploader.destroy(publicID)  
+        return {deleted:true}  
+    }
+    return {error:'no file like this'}
+    } catch (error) {
+        console.log(error);
+    return {error:error.message}
+        
+    }
+}
+
+async function deleteBulkFile(publicIDs){
+    try {
+        let publicID= publicIDs.length>0?publicIDs:false
+        console.log(publicID);
+    if (publicID!=false) {
+        for (let i = 0; i < publicID.length; i++) {
+            // const element = publicID[i];
+            await cloudinary.v2.uploader.destroy(publicID[i])
+        }
+          
+        return {deleted:true}  
+    }
+    return {error:'no public Ids'}
+    } catch (error) {
+        console.log(error);
+    return {error:error.message}
+        
+    }
 }
 
 async function deleteImg(publicIDs){
@@ -73,4 +142,4 @@ async function addpdf(file) {
       }
 }
 
-module.exports={uploadimg, deleteImg , deleteBulkImg, addpdf}
+module.exports={uploadimg, uploadVideo, uploadRaw, deleteFile, deleteBulkFile, deleteImg , deleteBulkImg, addpdf}
