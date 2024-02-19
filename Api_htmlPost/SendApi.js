00494e5be6012ff852fbbf8c66b9388fe580fc1c
Solutech11 <soluwizy@gmail.com>
2024-02-19 21:41:48 +0100

    update

--- Api_htmlPost/SendApi.js
+++ Api_htmlPost/SendApi.js
@@ -1,37 +1,43 @@
 // action="/school/forget" method="post" enctype="application/x-www-form-urlencoded" 
-// onsubmit="send(event, this,'msgforget','/login',true, 'Password Has been reset')"
+// onsubmit="send(event, this,'msgforget','/login',true, 'Password Has been reset',false)"
 
 
-function send(e,form,ouput,success, Alerter, Alerttext) {
-    ApiIntegration(form,ouput,success,Alerter,Alerttext)
+function send(e,form,ouput,success, Alerter, Alerttext, prompter) {
+    ApiIntegration(form,ouput,success,Alerter,Alerttext, prompter)
     e.preventDefault();
   }
 
-  async function ApiIntegration(form, output, success,Alerter, Alerttext) {
+  async function ApiIntegration(form, output, success,Alerter, Alerttext,prompter) {
     let msgg= document.getElementById(output)
-    try {
-        Loader.open()
-        console.log(form.enctpye);
-        let SendData=await axios({url:form.action, method:'post', headers:{
-            Accept:'application/json',
-            'Content-Type':`${form.enctype}`
-        }, data:new FormData(form)});
-        
-        let data= SendData.data
 
-        Alerter?alert(Alerttext):null
-
-        
-        window.location.pathname=success
-
-
-    Loader.close()
-    } catch (error) {
-        console.log(error);
-        msgg.innerHTML=error?.response?.data?error.response.data.Error:"Connection error"
-    } finally{
-        Loader.close()
-
-    }
     
+    const userConfirmed = prompter==true?window.confirm('Do you want to continue?. \n Do you want to continue? Click OK to continue, or Cancel to stop.'):true
+    if (userConfirmed) {
+        try {
+            Loader.open()
+            console.log(form.enctpye);
+            let SendData=await axios({url:form.action, method:'post', headers:{
+                Accept:'application/json',
+                'Content-Type':`${form.enctype}`
+            }, data:new FormData(form)});
+            
+            let data= SendData.data
+
+            Alerter?alert(Alerttext):null
+
+            
+            window.location.pathname=success
+
+
+        Loader.close()
+        } catch (error) {
+            console.log(error);
+            msgg.innerHTML=error?.response?.data?error.response.data.Error:"Connection error"
+        } finally{
+            Loader.close()
+
+        }
+    } else {
+        alert('You clicked Cancel. Action canceled.');
+    }
   }
